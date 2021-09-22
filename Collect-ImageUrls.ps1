270d5f3e0fd1d368ef84262530325b6e54bbc355
Tóth Attila <attila.toth@athalie.hu>
2021-09-22 10:26:08 +0200

    first commit

---
+++ Collect-ImageUrls.ps1
@@ -0,0 +1,28 @@
+﻿cls
+$ImagePath = "C:\Users\attila.toth\Documents\Azure DevOps\Web development\Purina\JPGimages"
+
+
+$files = Get-ChildItem -Path $ImagePath | where name -Like *.jpg
+
+$csv = Import-Csv -Path "C:\Users\attila.toth\Documents\Azure DevOps\Web development\Purina\Purina feed.csv"
+
+foreach($_ in $csv) {
+    
+    $NameSreachString = ($_.GTIN + "*")
+    $Images =Get-ChildItem -Path $ImagePath | where Name -Like $NameSreachString
+    
+    
+    foreach($Image in $Images) {
+        
+        $Urls += $Image.Fullname
+        $Urls += "|"
+
+    }
+    
+    $Urls = $Urls.Substring(0, $Urls.Length-1)
+    $_.Imageurl +=$Urls
+
+    Remove-Variable Urls
+    
+    
+}
---
+++ Rename-Files.ps1
@@ -0,0 +1,18 @@
+﻿
+$ImagePath = "C:\Users\attila.toth\Documents\Azure DevOps\Web development\Purina\images"
+
+
+$files = Get-ChildItem -Path $ImagePath | where name -Like *.jpg
+
+foreach($_ in $files) {
+    
+    Rename-Item -Path $_.Fullname -NewName ($_.Name).Replace(".tif", "")
+}
+
+
+$files = Get-ChildItem -Path $ImagePath | where name -Like *.jpg
+
+foreach($_ in $files) {
+    
+    Move-Item -Path $_.Fullname -Destination "C:\Users\attila.toth\Documents\Azure DevOps\Web development\Purina\JPGimages"
+}

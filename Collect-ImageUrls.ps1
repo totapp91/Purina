3176462d0d6cd21a87b83a1de140d2547b848bf3
Tóth Attila <attila.toth@athalie.hu>
2021-11-03 11:33:16 +0100

    Update result file

--- Collect-ImageUrls.ps1
+++ Collect-ImageUrls.ps1
@@ -31,5 +31,5 @@ foreach($_ in $csv) {
 }
 
 foreach ($_ in $csv) {
-    Export-Csv -InputObject $_ -Path "C:\Users\attila.toth\Documents\Azure DevOps\Web development\Purina\PurinaFeed_Pngimages.csv" -Append -Encoding utf8
+    Export-Csv -InputObject $_ -Path "C:\Users\attila.toth\Documents\Azure DevOps\Web development\Purina\PurinaFeed_images.csv" -Append -Encoding utf8
 }
